5502d0f0a144dbc08c7fe5cf76672b4db0272589
rgunjp <rishabh.gupta@scania.com>
2020-04-09 16:40:28 +0200

    first commit

---
+++ components/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import faker from 'faker';
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:00 pm" imageAvatar={faker.image.avatar()} content="This is a great movie" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Rish" timeAgo="Today at 5:00 pm" imageAvatar={faker.image.avatar()} content="This is a best film" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Today at 6:00 pm" imageAvatar={faker.image.avatar()} content="This is fantastic" />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDom.render(<App/>, document.querySelector('#root'))
---
+++ seasons/src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+    }
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // to set state use setState
+                this.setState({lat: position.coords.latitude})
+            },
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
+        );
+    }
+
+    render() {
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading...</div>
+    }
+}
+
+
+ReactDom.render(
+    <App/>,
+    document.querySelector("#root")
+);
